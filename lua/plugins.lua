d086cf5e2c034228235d01e0ce0a959588348b3e
efra <eliassaldarriaga65@outlook.es>
2023-04-14 00:29:16 -0500

    agregando plugins faltantes

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -25,17 +25,54 @@ return require('packer').startup(function(use)
         config = function()
             require("configs.gruvbox")
         end,
-})
+    })
+
+    -- Tree
+    use {
+      'nvim-tree/nvim-tree.lua',
+      requires = {
+        'nvim-tree/nvim-web-devicons', -- optional
+      },
+      config = function()
+        require("nvim-tree").setup {}
+      end
+    }
+
+    -- Tmux Navigation
+    use 'alexghergh/nvim-tmux-navigation'
+    
+    -- Terminal
+    use {"akinsho/toggleterm.nvim", tag = '*', config = function()
+        require("toggleterm").setup()
+      end
+    }
+
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+  -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+
+    -- Status Line
+    use 'nvim-lualine/lualine.nvim'
+
+    -- Bufferline
+    use {'akinsho/bufferline.nvim', tag = "v3.*", requires = 'nvim-tree/nvim-web-devicons'}
+
+    -- LSP
+    use 'neovim/nvim-lspconfig'
+
 
 
     -- File manager
-	use({
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v2.x",
-		requires = {
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons",
-			"MunifTanjim/nui.nvim",
-		},
-	})
+	--use({
+	--	"nvim-neo-tree/neo-tree.nvim",
+	--	branch = "v2.x",
+	--	requires = {
+	--		"nvim-lua/plenary.nvim",
+	--		"nvim-tree/nvim-web-devicons",
+	--		"MunifTanjim/nui.nvim",
+	--	},
+	--})
+    --
 end)
