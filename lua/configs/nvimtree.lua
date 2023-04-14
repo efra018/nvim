83688667e306441b8862dbffa4698d7c4ee34581
efra <eliassaldarriaga65@outlook.es>
2023-04-14 00:31:49 -0500

    agregando configuracion de nvimtree

---
+++ lua/configs/nvimtree.lua
@@ -0,0 +1,83 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+require("nvim-tree").setup({
+--    auto_close = true,
+})
+
+--require("nvim-tree").setup({
+--   open_on_setup = true,
+--   auto_close = true,
+--   sort_by = "case_sensitive",
+--   view = {
+--      width = 30,
+--      mappings = {
+--         list = {
+--            { key = "u", action = "dir_up" },
+--         },
+--      },
+--   },
+--   actions = {
+--      open_file = {
+--         quit_on_open = true,
+--      },
+--   },
+--   
+--   renderer = {
+--      group_empty = true,
+--      indent_width = 2,
+--      indent_markers = {
+--         enable = true,
+--         inline_arrows = true,
+--         icons = {
+--            corner = "└",
+--            edge = "¦",
+--            item = "¦",
+--            bottom = "─",
+--            none = " ",
+--         },
+--      },
+--      icons = {
+--         webdev_colors = true,
+--         git_placement = "before",
+--         modified_placement = "after",
+--         padding = " ",
+--         symlink_arrow = " ➛ ",
+--         show = {
+--            file = true,
+--            folder = true,
+--            folder_arrow = true,
+--            git = true,
+--            modified = true,
+--         },
+--         glyphs = {
+--            default = "",
+--            symlink = "",
+--            bookmark = "",
+--            modified = "●",
+--            folder = {
+--               --arrow_closed = "▶",
+--               --arrow_open = "▼",
+--               default = "",
+--               open = "",
+--               empty = "",
+--               empty_open = "",
+--               symlink = "",
+--               symlink_open = "",
+--            },
+--            git = {
+--               unstaged = "✗",
+--               staged = "✓",
+--               unmerged = "",
+--               renamed = "➜",
+--               untracked = "★",
+--               deleted = "",
+--               ignored = "◌",
+--            },
+--         },
+--      },  },
+--
+--      --filters = {
+--      --   dotfiles = true,
+--      --},
+--   })
