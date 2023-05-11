b046ff4686f13e40d087c52498dd4429cbd0cbab
efra <eliassaldarriaga65@outlook.es>
2023-05-11 00:47:03 -0500

    agregando contraste(hard)

--- lua/configs/gruvbox.lua
+++ lua/configs/gruvbox.lua
@@ -5,25 +5,25 @@ end
 
 gruvbox.setup({
     undercurl = true,
-  underline = true,
-  bold = true,
+    underline = true,
+    bold = true,
   --italic = false,
-  italic = {
-    strings = false,
-    operators = false,
-    comments = false,
-  },
-  strikethrough = true,
-  invert_selection = false,
-  invert_signs = false,
-  invert_tabline = false,
-  invert_intend_guides = false,
-  inverse = true, -- invert background for search, diffs, statuslines and errors
-  contrast = "", -- can be "hard", "soft" or empty string
-  palette_overrides = {},
-  overrides = {},
-  dim_inactive = false,
-  transparent_mode = false,
+    italic = {
+        strings = false,
+        operators = false,
+        comments = false,
+    },
+    strikethrough = true,
+    invert_selection = false,
+    invert_signs = false,
+    invert_tabline = false,
+    invert_intend_guides = false,
+    inverse = true, -- invert background for search, diffs, statuslines and errors
+    contrast = "hard", -- can be "hard", "soft" or empty string
+    palette_overrides = {},
+    overrides = {},
+    dim_inactive = false,
+    transparent_mode = false,
 })
 
 --require("gruvbox").setup({
