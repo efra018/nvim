a1234a464dde753b62a7280ce53a1585a8f3a106
efra <eliassaldarriaga65@outlook.es>
2023-05-11 00:46:19 -0500

    quitando palabra keyword_length

--- lua/configs/democmp.lua
+++ lua/configs/democmp.lua
@@ -62,8 +62,8 @@ cmp.setup({
       ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     }),
     sources = cmp.config.sources({
-        --{ name = 'nvim_lsp' },
-        { name = 'nvim_lsp', keyword_length = 3 },
+        { name = 'nvim_lsp' },
+        --{ name = 'nvim_lsp', keyword_length = 3 },
         --{ name = 'vsnip' }, -- For vsnip users.
         { name = 'luasnip' }, -- For luasnip users.
         { name = 'buffer' },
