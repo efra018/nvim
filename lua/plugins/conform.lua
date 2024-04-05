57cbf514dbd5ec66f3148c53a1f2bc40ab6c778f
efra <eliassaldarriaga65@outlook.es>
2024-04-05 17:50:31 -0500

    Editando archivo conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -6,7 +6,7 @@ return {
 		require("conform").setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
-        dart = { "dartls" },
+				--dart = { "dartls" },
 				--javascript = { 'prettierd' },
 				--typescript = { 'prettierd' },
 				--javascriptreact = { 'prettierd' },
