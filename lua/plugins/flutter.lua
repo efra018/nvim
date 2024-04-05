dbe2f062b67f4b474dc66c10ad867cc07376ba1e
efra <eliassaldarriaga65@outlook.es>
2024-04-05 17:51:30 -0500

    Editando configuracion para flutter-tools

--- lua/plugins/flutter.lua
+++ lua/plugins/flutter.lua
@@ -8,6 +8,7 @@ return {
 	},
 	config = function()
 		require("flutter-tools").setup({
+			require("telescope").load_extension("flutter"),
 			dart_path = "C:/flutter/bin/dart.bat",
 			flutter_path = "C:/flutter/bin/flutter.bat",
 			ui = {
@@ -18,24 +19,62 @@ return {
 				-- please note that this option is eventually going to be deprecated and users will need to
 				-- depend on plugins like `nvim-notify` instead.
 				-- notification_style = "native" | "plugin",
-				notification_style = "native",
+				notification_style = "plugin",
 			},
 			decorations = {
 				statusline = {
 					-- set to true to be able use the 'flutter_tools_decorations.app_version' in your statusline
 					-- this will show the current version of the flutter app from the pubspec.yaml file
-					app_version = false,
+					app_version = true,
 					-- set to true to be able use the 'flutter_tools_decorations.device' in your statusline
 					-- this will show the currently running device if an application was started with a specific
 					-- device
-					device = false,
+					device = true,
 					-- set to true to be able use the 'flutter_tools_decorations.project_config' in your statusline
 					-- this will show the currently selected project configuration
-					project_config = false,
+					project_config = true,
 				},
+			},
+			closing_tags = {
+				--highlight = "ErrorMsg", -- highlight for the closing tag
+				prefix = "//", -- character to use for close tag e.g. > Widget
+				enabled = true, -- set to false to disable
+			},
+			dev_log = {
+				enabled = true,
+				notify_errors = false, -- if there is an error whilst running then notify the user
+				open_cmd = "tabedit", -- command to use to open the log buffer
+			},
+			dev_tools = {
+				autostart = false, -- autostart devtools server if not detected
+				auto_open_browser = false, -- Automatically opens devtools in the browser
+			},
+			outline = {
+				open_cmd = "30vnew", -- command to use to open the outline buffer
+				auto_open = false, -- if true this will open the outline automatically when it is first populated
 			},
 			widget_guides = {
 				enabled = true,
+			},
+			lsp = {
+				--color = { -- show the derived colours for dart variables
+				--	enabled = false, -- whether or not to highlight color variables at all, only supported on flutter >= 2.10
+				--	background = false, -- highlight the background
+				--	background_color = nil, -- required, when background is transparent (i.e. background_color = { r = 19, g = 17, b = 24},)
+				--	foreground = false, -- highlight the foreground
+				--	virtual_text = true, -- show the highlight using virtual text
+				--	virtual_text_str = "■", -- the virtual text character to highlight
+				--},
+				-- see the link below for details on each option:
+				-- https://github.com/dart-lang/sdk/blob/master/pkg/analysis_server/tool/lsp_spec/README.md#client-workspace-configuration
+				settings = {
+					showTodos = true,
+					completeFunctionCalls = true,
+					analysisExcludedFolders = { "C:/flutter/packages" },
+					renameFilesWithClasses = "prompt", -- "always"
+					enableSnippets = true,
+					updateImportsOnRename = true, -- Whether to update imports and other directives when files are renamed. Required for `FlutterRename` command.
+				},
 			},
 		})
 	end,
